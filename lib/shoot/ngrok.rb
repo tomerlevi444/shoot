c83bf3c7efa0f6423e3b2f59eac1cdd9f4092dbf
Tomer Levi <tomerlevi@Tomers-MacBook-Pro.local>
2016-02-29 16:02:06 +0200

    use ngrok without subdomain

--- lib/shoot/ngrok.rb
+++ lib/shoot/ngrok.rb
@@ -10,7 +10,7 @@ module Shoot
     def initialize(port = 3000)
       @process = ChildProcess.build("ngrok", "-log=stdout", port.to_s)
       reader,writer = IO.pipe
-      process.io.stdout = writer
+      @process.io.stdout = writer
 
       start.tap do
         match = reader.readpartial(8192).match(/http:\/\/[a-z0-9A-Z]+.ngrok.com/).to_s
